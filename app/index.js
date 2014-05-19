0bef3b0157fd0b6576e4940c7b871ede25197e52
Matt Edelman <medelman@paypal.com>
2014-05-19 16:57:23 -0700

    updating

--- app/index.js
+++ app/index.js
@@ -4,6 +4,8 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 var banner = require('./lib/banner');
 var chalk = require('chalk');
+var fs = require('fs');
+//var _ = require('lodash');
 
 
 var NemoGenerator = yeoman.generators.Base.extend({
@@ -36,12 +38,28 @@ var NemoGenerator = yeoman.generators.Base.extend({
       name: 'browserOption',
       message: 'Which browser do you want to use by default?',
       default: "phantomjs",
-      "choices": ["phantomjs", "firefox", "chrome"]
-    }];
+      "choices": ["phantomjs", "firefox", "chrome", "safari"]
+    }, {
+      type: 'input',
+      name: 'seleniumJarPath',
+      message: 'Where is your selenium standalone Jar file? (Windows user? Provide windows style path)',
+      when: function(answers) {
+        var bo = answers.browserOption;
+        return (bo === "firefox" || bo === "safari");
+      },
+      validate: function(jarPath) {
+        var exists = fs.existsSync(jarPath);
+        if (!exists) {
+          return "Please make sure you've got a valid selenium standalone jar path specified! Please see https://github.com/paypal/nemo-docs/blob/master/driver-setup.md for more details.";
+        }
+        return true;
+      }
+    }, ];
 
     this.prompt(prompts, function(props) {
       this.baseDirOption = props.baseDirOption;
       this.browserOption = props.browserOption;
+      this.seleniumJarPath = props.seleniumJarPath;
 
       done();
     }.bind(this));
@@ -60,7 +78,13 @@ var NemoGenerator = yeoman.generators.Base.extend({
     var done = this.async();
     //base test directory
     this.mkdir(baseDir);
-    this.copy('loopmocha.js', taskDir + 'loopmocha.js');
+    //console.log("_", this._);
+    var _ = this._;
+    this._.templateSettings.imports.loscape = function(val) {
+      var newval = "<%=" + val + "%>";
+      return newval;
+    };
+    this.template('_loopmocha.js', taskDir + 'loopmocha.js');
     //config dir
     this.mkdir(configDir);
     this.copy('test/functional/config/nemo-plugins.json', configDir + '/nemo-plugins.json');
@@ -86,17 +110,46 @@ var NemoGenerator = yeoman.generators.Base.extend({
     // this.copy('_bower.json', 'bower.json');
   },
   installThings: function() {
-    var cmd = this.spawnCommand("npm", ["install", "--save-dev", "nemo@^v0.1.0", "nemo-mocha-factory@^v0.1.0", "grunt-loop-mocha@^v0.3.0", "nemo-drivex@^v0.1.0", "nconf@~v0.6.7", "xunit-file@v0.0.4"]);
+    var cmd = this.spawnCommand("npm", ["install", "--save-dev", "nemo@^v0.1.0", "nemo-view@^v0.1.0", "nemo-mocha-factory@^v0.1.0", "grunt-loop-mocha@^v0.3.0", "nemo-drivex@^v0.1.0", "nemo-locatex@^v0.1.0", "nconf@~v0.6.7", "xunit-file@v0.0.4"]);
     var done = this.async();
     cmd.on('close', function(code) {
       console.log('child process exited with code ' + code);
       done();
     });
   },
-  manualSteps: function() {
-    this.log(chalk.green('You\'re almost set. Please add the following line to your Gruntfile where you register tasks:'));
-    this.log(chalk.green('grunt.registerTask(\'automation\', [\'loopmocha:local\']);'));
+  editGruntfile: function() {
     var done = this.async();
+    //let's update Gruntfile.js if possible
+    fs.readFile('Gruntfile.js', 'utf8', function(err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      if (data.indexOf("loopmocha")) {
+        //already here?
+        done();
+        return;
+      }
+      var result = data.replace(/grunt.registerTask/, 'grunt.registerTask(\'automation\', [\'loopmocha:local\']);\ngrunt.registerTask');
+
+      fs.writeFile('Gruntfile.js', result, 'utf8', function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        done();
+      });
+    });
+  },
+  finalValidation: function() {
+    var done = this.async();
+    var browser = this.browserOption;
+    //if the browser is phantomjs or chrome, check for phantomjs or chromedriver
+    if (browser === "phantomjs" || browser === "chrome") {
+      var myBrowser = (browser === "phantomjs") ? ["PhantomJS", "PhantomJS"] : ["Chrome", "chromedriver"];
+      this.log(chalk.green("You selected " + myBrowser[0] + " as your browser. If you haven't already, please install " + myBrowser[1] + " to run tests locally."));
+      this.log(chalk.green("Please see"), chalk.underline.blue.bold("https://github.com/paypal/nemo-docs/blob/master/driver-setup.md"), chalk.green("for more details."));
+    }
+
+    this.log(chalk.green("Now try running 'grunt automation'"));
     done();
   },
 });
--- app/templates/_loopmocha.js
+++ app/templates/_loopmocha.js
@@ -9,33 +9,33 @@ module.exports = function loopmocha(grunt) {
   grunt.loadNpmTasks('grunt-loop-mocha');
   // Options
   return {
-    "src": ["<%=loopmocha.basedir%>/spec/*.js"],
+    "src": ["<%= loscape('loopmocha.basedir') %>/spec/*.js"],
     "basedir": process.cwd() + "/" + "test/functional",
     "options": {
       "mocha": {
-        "reportLocation": grunt.option("reportLocation") || "<%=loopmocha.basedir%>/report",
+        "reportLocation": grunt.option("reportLocation") || "<%= loscape('loopmocha.basedir') %>/report",
         "timeout": grunt.option("timeout") || 600000,
         "grep": grunt.option("grep") || 0,
         "debug": grunt.option("debug") || 0,
         "reporter": grunt.option("reporter") || "spec"
       },
       "nemoData": {
-        "autoBaseDir": "<%=loopmocha.basedir%>",
-        "targetBrowser": nconf.get("TARGET_BROWSER") || "firefox",
+        "autoBaseDir": "<%= loscape('loopmocha.basedir') %>",
+        "targetBrowser": nconf.get("TARGET_BROWSER") || "<%= browserOption %>",
+        <% if (seleniumJarPath) { %>
         "targetServer": nconf.get("TARGET_SERVER") || "localhost",
-        "targetBaseUrl": "http://localhost:8000",
-        "seleniumJar": nconf.get("SELENIUM_JAR") || "/usr/local/bin/selenium-standalone.jar",
+        "seleniumJar": nconf.get("SELENIUM_JAR") || "<%= seleniumJarPath %>",
         "serverProps": {
           "port": 4444
-        },
-        "stage": nconf.get("STAGE") || "STAGE2P1971"
+        }
+        <% } %>
       },
       "iterations": [{
         "description": "default"
       }]
     },
     "local": {
-      "src": "<%=loopmocha.src%>"
+      "src": "<%= loscape('loopmocha.src') %>"
     }
   };
 };
