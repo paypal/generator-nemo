0309dfdaed58f0c24a1f51a3ac2507dc36f500db
Nilesh Kulkarni <nikulkarni@paypal.com>
2015-03-29 23:06:19 -0400

    updated default wait time and messages

--- app/templates/test/functional/flow/navigate.js
+++ app/templates/test/functional/flow/navigate.js
@@ -10,28 +10,28 @@ var _enterLoginForm = function (nemo, user, pass) {
 }
 Navigate.prototype.loginSuccess = function(user, pass) {
   _enterLoginForm(this.nemo, user, pass);
-  return this.nemo.view.login.successWait();
+  return this.nemo.view.login.successWait(10000, 'Success message did not appear');
 };
 
 Navigate.prototype.loginFailure = function(user, pass) {
   _enterLoginForm(this.nemo, user, pass);
-  return this.nemo.view.login.failureWait();
+  return this.nemo.view.login.failureWait(10000, 'Failure message did not appear');
 
 };
 
 Navigate.prototype.logout = function() {
   this.nemo.view.nav.logoutLink().click();
-  return this.nemo.view.login.emailWaitVisible();
+  return this.nemo.view.login.emailWaitVisible(10000, 'Email did not appear');
 };
 
 Navigate.prototype.bank = function() {
   this.nemo.view.nav.bankLink().click();
-  return this.nemo.view.bank.numberWaitVisible();
+  return this.nemo.view.bank.numberWaitVisible(10000, 'Number did not appear');
 };
 
 Navigate.prototype.card = function() {
   this.nemo.view.nav.cardLink().click();
-  return nemo.view.card.numberWaitVisible();
+  return nemo.view.card.numberWaitVisible(10000, 'Number did not appear');
 };
 
 module.exports = Navigate;
--- app/templates/test/functional/flow/yreg.js
+++ app/templates/test/functional/flow/yreg.js
@@ -2,12 +2,12 @@
 module.exports = function yreg(nemo) {
   return {
     "registerYahooAccount": function() {
-      var yr = nemo.view.addView('yhooreg');
-      yr.fname().sendKeys('Matthew');
-      yr.lname().sendKeys('Edelman');
-      yr.bmonthOptionValue('11');
-      yr.bdayOptionValue('7');
-      yr.byearOptionValue('1974');
+      var yr = nemo.view.yhooreg;
+      yr.fname().sendKeys('MyFirstName');
+      yr.lname().sendKeys('MyLastName');
+      yr.bmonthOptionValue('1');
+      yr.bdayOptionValue('1');
+      yr.byearOptionValue('2000');
       return yr.maleradio().click();
     }
   };
